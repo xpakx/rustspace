29ff5954d6c1aa19118ed8bb6ff1790610f0ced7
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-06 19:59:39 +0100

    feat: add simple routes

--- rustspace/src/main.rs
+++ rustspace/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/index", get(root))
+        .route("/about", get(about))
+        .route("/help", get(help));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Homepage"
+}
+
+async fn about() -> &'static str {
+    "About us"
+}
+
+async fn help() -> &'static str {
+    "Help"
 }
