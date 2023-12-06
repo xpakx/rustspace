6b607d9e0f321d93276645e44fd5638f049688f1
xpakx <55586470+xpakx@users.noreply.github.com>
2023-12-06 20:13:47 +0100

    feat: return html

--- rustspace/src/main.rs
+++ rustspace/src/main.rs
@@ -1,4 +1,5 @@
 use axum::{routing::get, Router};
+use axum::response::Html;
 
 #[tokio::main]
 async fn main() {
@@ -7,18 +8,24 @@ async fn main() {
         .route("/index", get(root))
         .route("/about", get(about))
         .route("/help", get(help));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
 }
 
-async fn root() -> &'static str {
-    "Homepage"
+async fn root() -> Html<String> {
+    Html(format!("<h1>Homepage</h1>"))
 }
 
-async fn about() -> &'static str {
-    "About us"
+async fn about() -> Html<String> {
+    Html(format!("<h1>About us</h1>"))
 }
 
-async fn help() -> &'static str {
-    "Help"
+async fn help() -> Html<String> {
+    Html(format!("<h1>Help</h1>"))
 }
