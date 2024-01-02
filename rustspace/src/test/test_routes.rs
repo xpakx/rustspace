f0e07d381513ccbfe402c5531bdfb96eedb588ff
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-02 17:12:26 +0100

    test: extract test to separate files

---
+++ rustspace/src/test/test_routes.rs
@@ -0,0 +1,68 @@
+use axum::{extract::Request, body::{Body, to_bytes}, http::StatusCode};
+use tower::ServiceExt;
+
+use crate::test::prepare_server;
+
+#[tokio::test]
+async fn test_index() {
+    let response = prepare_server()
+        .await
+        .oneshot(
+            Request::builder()
+            .uri("/")
+            .body(Body::empty())
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("Welcome!"));
+    assert!(content.contains("Homepage"));
+}
+
+#[tokio::test]
+async fn test_about() {
+    let response = prepare_server()
+        .await
+        .oneshot(
+            Request::builder()
+            .uri("/about")
+            .body(Body::empty())
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("About us"));
+}
+
+#[tokio::test]
+async fn test_help() {
+    let response = prepare_server()
+        .await
+        .oneshot(
+            Request::builder()
+            .uri("/help")
+            .body(Body::empty())
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("Help"));
+}
