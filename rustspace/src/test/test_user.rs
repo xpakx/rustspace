2a719d7860ca402b867cecc8d4bbff3eb989df6c
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-03 16:59:34 +0100

    test: test validating email while updating

--- rustspace/src/test/test_user.rs
+++ rustspace/src/test/test_user.rs
@@ -1,7 +1,8 @@
 use axum::{extract::Request, body::{Body, to_bytes}, http::StatusCode};
 use tower::ServiceExt;
+use serial_test::serial;
 
-use crate::test::prepare_server;
+use crate::{test::{prepare_server, prepare_server_with_user, prepare_server_with_db}, security::get_token, db::get_db};
 
 #[tokio::test]
 async fn test_getting_email_form() {
@@ -45,4 +46,72 @@ async fn test_getting_password_form() {
     let content = std::str::from_utf8(&*bytes).unwrap();
     assert!(content.contains("<form"));
     assert!(content.contains("password"));
+}
+
+#[tokio::test]
+#[serial]
+async fn test_changing_email_to_wrong_value() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("PUT")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .header("Cookie", format!("Token={};", token))
+            .uri("/password")
+            .body(Body::from("email=testveryveryververyveryveryververyveryveryververyveryveryververyveryveryververylongemail%40email.com"))
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+}
+
+#[tokio::test]
+#[serial]
+async fn test_changing_email_to_duplicated_one() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+
+    let db = get_db("postgresql://root:password@localhost:5432/rustspacetest").await;
+
+    _ = sqlx::query("INSERT INTO users (screen_name, email, password) VALUES ($1, $2, $3)")
+        .bind("Test")
+        .bind("test@email.com")
+        .bind("password")
+        .execute(&db)
+        .await;
+    _ = sqlx::query("INSERT INTO users (screen_name, email, password) VALUES ($1, $2, $3)")
+        .bind("Second")
+        .bind("in_use@email.com")
+        .bind("password")
+        .execute(&db)
+        .await;
+    let response = prepare_server_with_db(db)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("PUT")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .header("Cookie", format!("Token={};", token))
+            .uri("/password")
+            .body(Body::from("email=in_use%40email.com"))
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+    assert!(content.contains("unique"));
 }
