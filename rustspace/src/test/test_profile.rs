863fc07286f8ebfb69c33fbe01d7c270be83b109
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-05 23:09:29 +0100

    test: test visiting profile

--- rustspace/src/test/test_profile.rs
+++ rustspace/src/test/test_profile.rs
@@ -1,7 +1,7 @@
 use axum::{extract::Request, body::{Body, to_bytes}, http::StatusCode};
 use tower::ServiceExt;
 
-use crate::test::prepare_server;
+use crate::test::{prepare_server, prepare_server_with_user};
 
 #[tokio::test]
 async fn test_getting_nonexistent_profile() {
@@ -22,4 +22,25 @@ async fn test_getting_nonexistent_profile() {
     let bytes = body.unwrap();
     let content = std::str::from_utf8(&*bytes).unwrap();
     assert!(content.contains("no such user"));
+}
+
+#[tokio::test]
+async fn test_getting_profile() {
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .uri("/profile/Test")
+            .body(Body::empty())
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 2000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("Test's profile"));
 }
