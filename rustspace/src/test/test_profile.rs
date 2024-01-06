59685c8125d486b908fc5de9f2f1b066a4c8d4ec
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-06 19:31:05 +0100

    test: make sure that profile owner's see button for edition

--- rustspace/src/test/test_profile.rs
+++ rustspace/src/test/test_profile.rs
@@ -1,9 +1,11 @@
 use axum::{extract::Request, body::{Body, to_bytes}, http::StatusCode};
 use tower::ServiceExt;
+use serial_test::serial;
 
-use crate::test::{prepare_server, prepare_server_with_user};
+use crate::{test::{prepare_server, prepare_server_with_user}, security::get_token};
 
 #[tokio::test]
+#[serial]
 async fn test_getting_nonexistent_profile() {
     let response = prepare_server()
         .await
@@ -25,6 +27,7 @@ async fn test_getting_nonexistent_profile() {
 }
 
 #[tokio::test]
+#[serial]
 async fn test_getting_profile() {
     let response = prepare_server_with_user(false)
         .await
@@ -43,4 +46,29 @@ async fn test_getting_profile() {
     let bytes = body.unwrap();
     let content = std::str::from_utf8(&*bytes).unwrap();
     assert!(content.contains("Test's profile"));
+}
+
+#[tokio::test]
+#[serial]
+async fn test_getting_profile_by_owner() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .uri("/profile/Test")
+            .header("Cookie", format!("Token={};", token))
+            .body(Body::empty())
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 2000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("Test's profile"));
+    assert!(content.contains("Edit"));
 }
