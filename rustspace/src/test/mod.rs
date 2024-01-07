99d3a15c0148e5d266f8c92ff7de554ca96397b3
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-07 16:42:19 +0100

    test: tidy up test server configuration

--- rustspace/src/test/mod.rs
+++ rustspace/src/test/mod.rs
@@ -10,13 +10,15 @@ mod test_auth;
 mod test_user;
 mod test_profile;
 
+async fn clear_db(db: &PgPool) {
+    _ = sqlx::query("DELETE FROM users")
+        .execute(db)
+        .await;
+}
+
 async fn prepare_server() -> axum::Router {
     let db = get_db("postgresql://root:password@localhost:5432/rustspacetest").await;
-
-    _ = sqlx::query("DELETE FROM users")
-        .execute(&db)
-        .await;
-
+    clear_db(&db).await;
     let app = get_router()
         .with_state(Arc::new(AppState{db}));
     app
@@ -24,26 +26,8 @@ async fn prepare_server() -> axum::Router {
 
 async fn prepare_server_with_user(hash_password: bool) -> axum::Router {
     let db = get_db("postgresql://root:password@localhost:5432/rustspacetest").await;
-
-    _ = sqlx::query("DELETE FROM users")
-        .execute(&db)
-        .await;
-
-    let salt = SaltString::generate(&mut OsRng);
-    let password = match hash_password {
-        true => Argon2::default()
-            .hash_password("password".as_bytes(), &salt)
-            .map(|hash| hash.to_string()).unwrap(),
-        false => String::from("password")
-    };
-
-    _ = sqlx::query("INSERT INTO users (screen_name, email, password) VALUES ($1, $2, $3)")
-        .bind("Test")
-        .bind("test@email.com")
-        .bind(password)
-        .execute(&db)
-        .await;
-
+    clear_db(&db).await;
+    insert_default_user(hash_password, &db).await;
     let app = get_router()
         .with_state(Arc::new(AppState{db}));
     app
@@ -51,10 +35,7 @@ async fn prepare_server_with_user(hash_password: bool) -> axum::Router {
 
 async fn prepare_db() -> PgPool {
     let db = get_db("postgresql://root:password@localhost:5432/rustspacetest").await;
-
-    _ = sqlx::query("DELETE FROM users")
-        .execute(&db)
-        .await;
+    clear_db(&db).await;
     db
 }
 
@@ -64,3 +45,21 @@ async fn prepare_server_with_db(db: PgPool) -> axum::Router {
     app
 }
 
+async fn insert_default_user(hash_password: bool, db: &PgPool) {
+    let password = match hash_password {
+        true => {
+            let salt = SaltString::generate(&mut OsRng);
+            Argon2::default()
+            .hash_password("password".as_bytes(), &salt)
+            .map(|hash| hash.to_string()).unwrap()
+        },
+        false => String::from("password")
+    };
+
+    _ = sqlx::query("INSERT INTO users (screen_name, email, password) VALUES ($1, $2, $3)")
+        .bind("Test")
+        .bind("test@email.com")
+        .bind(password)
+        .execute(db)
+        .await;
+}
