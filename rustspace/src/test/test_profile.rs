f3885c24117b4cc54439a0f0818cc570a9246db1
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-09 16:47:50 +0100

    test: test profile updates

--- rustspace/src/test/test_profile.rs
+++ rustspace/src/test/test_profile.rs
@@ -1,8 +1,9 @@
 use axum::{extract::Request, body::{Body, to_bytes}, http::StatusCode};
+use sqlx::{PgPool, Postgres};
 use tower::ServiceExt;
 use serial_test::serial;
 
-use crate::{test::{prepare_server, prepare_server_with_user, prepare_server_with_db, prepare_db, insert_default_user, clear_profiles}, security::get_token};
+use crate::{test::{prepare_server, prepare_server_with_user, prepare_server_with_db, prepare_db, insert_default_user, clear_profiles}, security::get_token, UserModel};
 
 #[tokio::test]
 #[serial]
@@ -173,4 +174,54 @@ async fn test_changing_profile_without_profile() {
     let bytes = body.unwrap();
     let content = std::str::from_utf8(&*bytes).unwrap();
     assert!(content.contains("London"));
+}
+
+async fn insert_default_profile(db: &PgPool) {
+    let user_db = sqlx::query_as::<Postgres, UserModel>(
+        "SELECT * FROM users WHERE screen_name = $1",
+        )
+        .bind("Test")
+        .fetch_optional(db)
+        .await;
+    let user_id = user_db.unwrap().unwrap().id.unwrap();
+
+    _ = sqlx::query("INSERT INTO profiles (gender, city, real_name, description, user_id) VALUES ($1, $2, $3, $4m $5)")
+        .bind("male")
+        .bind("London")
+        .bind("James Bond")
+        .bind("007")
+        .bind("test@email.com")
+        .bind(user_id)
+        .execute(db)
+        .await;
+}
+
+#[tokio::test]
+#[serial]
+async fn test_changing_profile_with_existing_profile() {
+    let db = prepare_db().await;
+    insert_default_user(false, &db).await;
+    insert_default_profile(&db).await;
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_db(db.clone())
+        .await
+        .oneshot(
+            Request::builder()
+            .method("PUT")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .header("Cookie", format!("Token={};", token))
+            .uri("/profile")
+            .body(Body::from("gender=&city=Cambridge&description=&real_name="))
+            .unwrap()
+            )
+        .await
+        .unwrap();
+    
+    clear_profiles(&db).await;
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("Cambridge"));
 }
