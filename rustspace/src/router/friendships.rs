2c7e872e570dde6855d1588f25248443b965b9f5
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-24 19:24:05 +0100

    feat: improve error messages for sending friendship request

--- rustspace/src/router/friendships.rs
+++ rustspace/src/router/friendships.rs
@@ -58,24 +58,35 @@ pub async fn send_friend_request(
         return HtmlTemplate(template).into_response()
     };
 
-    if let Some(_) = friendship {
-        let template = ErrorsTemplate {errors: vec!["Request already created!"]};
-        return HtmlTemplate(template).into_response()
-    } else {
-        let query_result =
-            sqlx::query("INSERT INTO friendships (user_id, friend_id) VALUES ($1, $2)")
-            .bind(&user.id)
-            .bind(&friend.id)
-            .execute(&state.db)
-            .await
-            .map_err(|err: sqlx::Error| err.to_string());
+    match friendship {
+        Some(accepted) if accepted.accepted =>  {
+            let template = ErrorsTemplate {errors: vec!["You're already friends!"]};
+            return HtmlTemplate(template).into_response()
+        },
+        Some(rejected) if rejected.rejected => {
+            let template = ErrorsTemplate {errors: vec!["User already have rejected your request!"]};
+            return HtmlTemplate(template).into_response()
+        },
+        Some(_) => {
+            let template = ErrorsTemplate {errors: vec!["Request already created!"]};
+            return HtmlTemplate(template).into_response()
+        },
+        None => {
+            let query_result =
+                sqlx::query("INSERT INTO friendships (user_id, friend_id) VALUES ($1, $2)")
+                .bind(&user.id)
+                .bind(&friend.id)
+                .execute(&state.db)
+                .await
+                .map_err(|err: sqlx::Error| err.to_string());
 
-        if let Err(_) = query_result {
-            let template = ErrorsTemplate {errors: vec!["Couldn't create profile!"]};
+            if let Err(_) = query_result {
+                let template = ErrorsTemplate {errors: vec!["Couldn't send request!"]};
+                return HtmlTemplate(template).into_response()
+            }
+            info!("request succesfully created.");
+            let template = ErrorsTemplate {errors: vec!["TODO"]};
             return HtmlTemplate(template).into_response()
         }
-        info!("profile succesfully created.");
-        let template = ErrorsTemplate {errors: vec!["TODO"]};
-        return HtmlTemplate(template).into_response()
     }
 }
