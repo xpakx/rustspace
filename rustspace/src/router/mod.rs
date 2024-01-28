0cee12eb55cf7cf74bf06421c679747e6af27096
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-28 21:56:30 +0100

    feat: add new endpoints for friends and frienship requests pagination

--- rustspace/src/router/mod.rs
+++ rustspace/src/router/mod.rs
@@ -7,7 +7,7 @@ use crate::AppState;
 use self::{
     main::{root, about, help},
     user::{user_page, register_form, register_user, check_password, check_username, check_email, check_password_repeat, login_form, login, logout, to_login, edit_email, edit_password, update_email, update_password, edit_avatar, upload_avatar, delete_avatar}, 
-    profile::{profile, edit_profile, update_profile}, community::{community, get_users_page, search_users, get_search_users_page}, friendships::{send_friend_request, friends, requests, change_request_state}
+    profile::{profile, edit_profile, update_profile}, community::{community, get_users_page, search_users, get_search_users_page}, friendships::{send_friend_request, friends, requests, change_request_state, requests_page, friends_page}
 };
 mod main;
 mod user;
@@ -48,6 +48,8 @@ pub fn get_router() -> Router<Arc<AppState>> {
         .route("/avatar", delete(delete_avatar))
         .route("/friendships", post(send_friend_request))
         .route("/friends", get(friends))
+        .route("/friends/page", get(friends_page))
         .route("/friends/requests", get(requests))
+        .route("/friends/requests/page", get(requests_page))
         .route("/friends/requests/:id", post(change_request_state))
 }
