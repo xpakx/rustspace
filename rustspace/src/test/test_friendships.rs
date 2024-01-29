f96c0c99d90c47fdb8d83d1ca8148e735aeef206
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-29 15:00:09 +0100

    test: test user trying to befriend themself

--- rustspace/src/test/test_friendships.rs
+++ rustspace/src/test/test_friendships.rs
@@ -82,3 +82,29 @@ async fn test_making_friendship_request_with_empty_name() {
     assert!(content.contains("username"));
     assert!(content.contains("empty"));
 }
+
+#[tokio::test]
+#[serial]
+async fn test_user_trying_to_befriend_themself() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("POST")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .uri("/friendships")
+            .body(Body::from("username=Test"))
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+    assert!(content.contains("befriend yourself"));
+}
