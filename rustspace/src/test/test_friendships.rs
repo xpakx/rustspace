1e49d322d61d9b0fa23dac3080c0613a63e9e329
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-29 15:38:51 +0100

    test: make sure authenticated users can access /friends endpoint

--- rustspace/src/test/test_friendships.rs
+++ rustspace/src/test/test_friendships.rs
@@ -259,7 +259,7 @@ async fn test_making_request() {
     clear_friendships(&db).await;
 }
 
-// frienship requests view
+// friendship requests view
 
 #[tokio::test]
 #[serial]
@@ -295,6 +295,55 @@ async fn test_if_friendship_requests_endpoint_exists() {
             .method("GET")
             .header("Cookie", format!("Token={};", token))
             .uri("/friends/requests")
+            .body(Body::empty())
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("Requests"));
+}
+
+// friends view
+
+#[tokio::test]
+#[serial]
+async fn test_getting_friends_by_unauthenticated_user() {
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("GET")
+            .uri("/friends")
+            .body(Body::empty())
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+    assert!(content.contains("Authentication Error"));
+}
+
+#[tokio::test]
+#[serial]
+async fn test_if_friends_endpoint_exists() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("GET")
+            .header("Cookie", format!("Token={};", token))
+            .uri("/friends")
             .body(Body::empty())
             .unwrap()
             )
