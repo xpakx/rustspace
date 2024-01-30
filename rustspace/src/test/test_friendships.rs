d123cd3dd658cb4c244e5eccd9a4cb6d8380b3f4
xpakx <55586470+xpakx@users.noreply.github.com>
2024-01-30 16:53:41 +0100

    test: test accepting non-existing friend request

--- rustspace/src/test/test_friendships.rs
+++ rustspace/src/test/test_friendships.rs
@@ -538,3 +538,29 @@ async fn test_changing_request_state_with_wrong_state() {
     assert!(content.contains("error"));
     assert!(content.contains("Unsupported state"));
 }
+
+#[tokio::test]
+#[serial]
+async fn test_accepting_nonexistent_request() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("POST")
+            .uri("/friends/requests/1")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .header("Cookie", format!("Token={};", token))
+            .body(Body::from("state=accepted"))
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+    assert!(content.contains("No such request"));
+}
