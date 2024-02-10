68da0fd4a55e6772386e5e394cdf8b0449cec343
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-10 17:01:02 +0100

    test: test deleting other user's post

--- rustspace/src/test/test_post.rs
+++ rustspace/src/test/test_post.rs
@@ -1,9 +1,11 @@
 use axum::{extract::Request, body::{Body, to_bytes}, http::StatusCode};
-use sqlx::Row;
+use sqlx::{Row, PgPool, Postgres};
 use tower::ServiceExt;
 use serial_test::serial;
 
-use crate::{test::{prepare_server_with_user, prepare_db, prepare_server_with_db, insert_new_user, clear_posts}, security::get_token};
+use crate::{test::{prepare_server_with_user, prepare_db, prepare_server_with_db, insert_new_user, clear_posts}, security::get_token, UserModel};
+
+// adding post
 
 #[tokio::test]
 #[serial]
@@ -161,19 +163,38 @@ async fn test_adding_post() {
         .await
         .unwrap();
 
-    assert_eq!(response.status(), StatusCode::OK);
-
     let result = sqlx::query("SELECT COUNT(*) FROM posts")
         .fetch_one(&db)
         .await;
     clear_posts(&db).await;
 
+    assert_eq!(response.status(), StatusCode::OK);
     assert!(result.is_ok());
     if let Ok(result) = result {
         assert_eq!(result.get::<i64, _>(0), 1);
     }
 }
+
 // deleting post
+
+async fn insert_post(username: &str, title: &str, content: &str, db: &PgPool) -> i32 {
+    let user_db = sqlx::query_as::<Postgres, UserModel>("SELECT * FROM users WHERE screen_name = $1")
+        .bind(&username)
+        .fetch_optional(db)
+        .await;
+    let Ok(Some(user)) = user_db else {
+        panic!("No such user!");
+    };
+
+    let result = sqlx::query_scalar("INSERT INTO posts (user_id, title, content) VALUES ($1, $2, $3) RETURNING id")
+        .bind(&user.id)
+        .bind(title)
+        .bind(content)
+        .fetch_one(db)
+        .await;
+
+    result.unwrap()
+}
 
 #[tokio::test]
 #[serial]
@@ -224,4 +245,35 @@ async fn test_deleting_nonexistent_post() {
     let content = std::str::from_utf8(&*bytes).unwrap();
     assert!(content.contains("error"));
     assert!(content.contains("No such post"));
+}
+
+#[tokio::test]
+#[serial]
+async fn test_deleting_post_authored_by_different_user() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let db = prepare_db().await;
+    insert_new_user("User", "user@mail.com", &db).await;
+    insert_new_user("Test", "test@mail.com", &db).await;
+    let post_id = insert_post("User", "Title", "Content", &db).await;
+    let response = prepare_server_with_db(db.clone())
+        .await
+        .oneshot(
+            Request::builder()
+            .method("DELETE")
+            .header("Cookie", format!("Token={};", token))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .uri(format!("/blog/{}", post_id))
+            .body(Body::empty())
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+    assert!(content.contains("cannot delete"));
 }
