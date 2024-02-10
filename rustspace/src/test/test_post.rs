7a1390fc7290a0b6eb7a3e2273689d1dc325a7c8
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-10 16:21:21 +0100

    test: test post validation while adding

--- rustspace/src/test/test_post.rs
+++ rustspace/src/test/test_post.rs
@@ -28,3 +28,115 @@ async fn test_making_post_request_by_unauthenticated_user() {
     assert!(content.contains("error"));
     assert!(content.contains("Unauthenticated"));
 }
+
+#[tokio::test]
+#[serial]
+async fn test_making_post_request_with_no_title() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("POST")
+            .header("Cookie", format!("Token={};", token))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .uri("/blog")
+            .body(Body::from("content=content"))
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+    assert!(content.contains("title"));
+    assert!(content.contains("empty"));
+}
+
+#[tokio::test]
+#[serial]
+async fn test_making_post_request_with_empty_title() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("POST")
+            .header("Cookie", format!("Token={};", token))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .uri("/blog")
+            .body(Body::from("title=&content=content"))
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+    assert!(content.contains("title"));
+    assert!(content.contains("empty"));
+}
+
+#[tokio::test]
+#[serial]
+async fn test_making_post_request_with_no_content() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("POST")
+            .header("Cookie", format!("Token={};", token))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .uri("/blog")
+            .body(Body::from("title=title"))
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+    assert!(content.contains("content"));
+    assert!(content.contains("empty"));
+}
+
+#[tokio::test]
+#[serial]
+async fn test_making_post_request_with_empty_content() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("POST")
+            .header("Cookie", format!("Token={};", token))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .uri("/blog")
+            .body(Body::from("title=title&content="))
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+    assert!(content.contains("content"));
+    assert!(content.contains("empty"));
+}
