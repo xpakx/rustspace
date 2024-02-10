5317e6ef9b1043f77e6216c6b268305960b5a2e1
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-10 17:31:25 +0100

    test: test editing non-existent post

--- rustspace/src/test/test_post.rs
+++ rustspace/src/test/test_post.rs
@@ -342,3 +342,30 @@ async fn test_editing_post_by_unauthenticated_user() {
     assert!(content.contains("error"));
     assert!(content.contains("Unauthenticated"));
 }
+
+#[tokio::test]
+#[serial]
+async fn test_editing_nonexistent_post() {
+    let (token, _) = get_token(&Some(String::from("Test")));
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("PUT")
+            .header("Cookie", format!("Token={};", token))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .uri("/blog/1")
+            .body(Body::from("title=title&content=content"))
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("error"));
+    assert!(content.contains("No such post"));
+}
