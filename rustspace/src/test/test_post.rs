9d02fca31dabd146b977eb3e31975f24b079c65c
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-10 17:52:55 +0100

    test: test getting non-existent post

--- rustspace/src/test/test_post.rs
+++ rustspace/src/test/test_post.rs
@@ -568,3 +568,27 @@ async fn test_editing_post() {
         }
     }
 }
+
+// getting post
+
+#[tokio::test]
+#[serial]
+async fn test_getting_nonexistent_post() {
+    let response = prepare_server_with_user(false)
+        .await
+        .oneshot(
+            Request::builder()
+            .method("GET")
+            .uri("/blog/1")
+            .body(Body::empty())
+            .unwrap()
+            )
+        .await
+        .unwrap();
+
+    let body = to_bytes(response.into_body(), 1000).await;
+    assert!(body.is_ok());
+    let bytes = body.unwrap();
+    let content = std::str::from_utf8(&*bytes).unwrap();
+    assert!(content.contains("no such post"));
+}
